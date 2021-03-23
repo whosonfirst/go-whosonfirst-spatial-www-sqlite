2f22cc3c017d9f3b6e02b9269d381f5098a403e0
thisisaaronland <devnull@localhost>
2021-03-22 17:04:17 -0700

    update to use whosonfirst/go-whosonfirst-spatial-www v0.0.23

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,44 +2,24 @@ package main
 
 import (
 	"context"
-	www_flags "github.com/whosonfirst/go-whosonfirst-spatial-www/flags"
 	"github.com/whosonfirst/go-whosonfirst-spatial-www/server"
 	_ "github.com/whosonfirst/go-whosonfirst-spatial-sqlite"
-	spatial_flags "github.com/whosonfirst/go-whosonfirst-spatial/flags"
 	"log"
-	"github.com/sfomuseum/go-flags/flagset"
 )
 
 func main() {
 
 	ctx := context.Background()
 
-	fs, err := spatial_flags.CommonFlags()
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = spatial_flags.AppendIndexingFlags(fs)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = www_flags.AppendWWWFlags(fs)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	flagset.Parse(fs)
-
 	app, err := server.NewHTTPServerApplication(ctx)
 
-	err = app.RunWithFlagSet(ctx, fs)
-
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	err = app.Run(ctx)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
